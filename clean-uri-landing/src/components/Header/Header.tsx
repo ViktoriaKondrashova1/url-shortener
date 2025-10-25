9878a1da3716459133af964627a878e017b05ede
Viktoria Kondrashova <kavidmi@gmail.com>
2025-10-25 18:12:25 +0300

    chore: install react router dom

--- clean-uri-landing/src/components/Header/Header.tsx
+++ clean-uri-landing/src/components/Header/Header.tsx
@@ -1,17 +1,27 @@
-import { ButtonGroup } from '@chakra-ui/react';
-import { APP_LOGO } from '../../constants/constants';
+import { ButtonGroup, HStack, Link, Flex, Spacer } from '@chakra-ui/react';
+import { APP_LOGO, MENU_ITEMS } from '../../constants/constants';
 import { AppButton } from '../Button/Button';
 import { Logo } from '../Logo/Logo';
 import './Header.scss';
 
 export const Header = () => {
   return (
-    <header className='header'>
-      <Logo text={APP_LOGO} />
+    <Flex className='header'>
+      <Flex gap="16">
+        <Logo text={APP_LOGO} />
+        <HStack as='nav' gap={8} className='header-menu'>
+          {MENU_ITEMS.map(el => (
+            <Link key={el.id} href={el.href}>
+              {el.text}
+            </Link>
+          ))}
+        </HStack>
+      </Flex>
+      <Spacer />
       <ButtonGroup size='sm'>
         <AppButton className='login-btn' rounded='full' text='Login' onClick={() => {}} />
         <AppButton rounded='full' text='Sign Up' onClick={() => {}} />
       </ButtonGroup>
-    </header>
+    </Flex>
   );
 };
--- clean-uri-landing/src/components/Logo/Logo.tsx
+++ clean-uri-landing/src/components/Logo/Logo.tsx
@@ -1,10 +1,22 @@
 import { Heading } from '@chakra-ui/react';
+//import { useNavigate } from 'react-router-dom';
 import { FC } from 'react';
+//import { ROUTES } from '../../constants/constants';
 
 interface LogoProps {
   text: string;
 }
 
 export const Logo: FC<LogoProps> = ({ text }) => {
-  return <Heading size='5xl' fontWeight="bold">{text}</Heading>;
+  //const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    //navigate(ROUTES.main);
+  };
+
+  return (
+    <Heading size='5xl' fontWeight='bold' onClick={handleLogoClick} cursor="pointer">
+      {text}
+    </Heading>
+  );
 };
