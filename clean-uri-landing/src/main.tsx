e6f446d6ebdd876b7c3e0f2fff094a5c485f65c1
Viktoria Kondrashova <kavidmi@gmail.com>
2025-10-31 19:05:31 +0300

    feat: add chakra theme config

--- clean-uri-landing/src/main.tsx
+++ clean-uri-landing/src/main.tsx
@@ -1,14 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
+import { ChakraProvider, createSystem, defaultConfig } from '@chakra-ui/react';
+import { config } from './styles/theme.ts';
 import './styles/index.scss';
 
-
+const system = createSystem(defaultConfig, config);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ChakraProvider value={defaultSystem}>
+    <ChakraProvider value={system}>
       <App />
     </ChakraProvider>
   </StrictMode>
---
+++ clean-uri-landing/src/styles/theme.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from '@chakra-ui/react';
+
+export const config = defineConfig({
+  theme: {
+    breakpoints: {
+      sm: '320px',
+      md: '768px',
+      lg: '960px',
+      xl: '1200px',
+    },
+    tokens: {
+      colors: {
+        blue: { value: 'hsl(180, 66%, 49%)' },
+        purple: { value: 'hsl(257, 27%, 26%)' },
+        gray: {
+          400: { value: 'hsl(0, 0%, 75%)' },
+          500: { value: 'hsl(257, 7%, 63%)' },
+          900: { value: 'hsl(255, 11%, 22%)' },
+          950: { value: 'hsl(260, 8%, 14%)' },
+        },
+      },
+    },
+  },
+});
