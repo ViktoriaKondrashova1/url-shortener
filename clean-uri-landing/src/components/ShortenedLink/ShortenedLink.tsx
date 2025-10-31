bc2783f60dbd9c391b2c9cd8a2a04a5cbda03a7b
Viktoria Kondrashova <kavidmi@gmail.com>
2025-10-31 19:05:48 +0300

    feat: add copy function

--- clean-uri-landing/src/components/ShortenedLink/ShortenedLink.tsx
+++ clean-uri-landing/src/components/ShortenedLink/ShortenedLink.tsx
@@ -5,14 +5,29 @@ import { LinksBlockProps } from 'types/types';
 import './ShortenedLink.scss';
 
 export const ShortenedLink: FC<LinksBlockProps> = link => {
+  const { originalLink, shortenedLink } = link;
   const [isButtonClicked, setisButtonClicked] = useState<boolean>(false);
+
+  const handleCopyClick = async (link: string): Promise<void> => {
+    try {
+      await navigator.clipboard.writeText(link);
+      setisButtonClicked(true);
+      setTimeout(() => setisButtonClicked(false), 2000);
+    } catch (err) {
+      console.error('Failed to copy link:', err);
+    }
+  };
 
   return (
     <HStack borderRadius='md' className='link-block' gap={10}>
-      <Text className='original-link'>{link.originalLink}</Text>
+      <Text className='original-link'>{originalLink}</Text>
       <HStack gap={8}>
-        <Text className='shortened'>{link.shortenedLink}</Text>
-        <AppButton text={isButtonClicked ? 'Copied!' : 'Copy'} onClick={() => {}} />
+        <Text className='shortened'>{shortenedLink}</Text>
+        <AppButton
+          text={isButtonClicked ? 'Copied!' : 'Copy'}
+          disabled={isButtonClicked}
+          onClick={() => handleCopyClick(shortenedLink)}
+        />
       </HStack>
     </HStack>
   );
