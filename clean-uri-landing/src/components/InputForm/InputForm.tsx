5421752f6d675602129473e4989cdd7a08fe0863
Viktoria Kondrashova <kavidmi@gmail.com>
2025-11-03 13:28:56 +0300

    feat: add controlled input form

--- clean-uri-landing/src/components/InputForm/InputForm.tsx
+++ clean-uri-landing/src/components/InputForm/InputForm.tsx
@@ -1,18 +1,32 @@
 import { HStack, Input } from '@chakra-ui/react';
 import { AppButton } from '../Button/Button';
 import { getShortenedURL } from '../../api/getShortenedURL';
+import { ChangeEvent, useState } from 'react';
 import './InputForm.scss';
 
 export const InputForm = () => {
+  const [inputValue, setInputValue] = useState<string>('');
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
   const handleButtonClick = async () => {
-    const link = 'https://chakra-ui.com/docs/components/menu';
+    const link = inputValue.trim();
     const result = await getShortenedURL(link);
-    console.log(result);
+    console.log('shortened: ', result);
   };
 
   return (
     <HStack gap={8} borderRadius='md' className='form'>
-      <Input placeholder='Shorten a link here...' size='lg' borderRadius='md' bg='white' />
+      <Input
+        placeholder='Shorten a link here...'
+        size='lg'
+        borderRadius='md'
+        bg='white'
+        value={inputValue}
+        onChange={e => handleInputChange(e)}
+      />
       <AppButton rounded='md' text=' Shorten It!' onClick={handleButtonClick} />
     </HStack>
   );
