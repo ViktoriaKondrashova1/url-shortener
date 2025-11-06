eb15818403e1e7ad2649000360e665f0d83870ef
Viktoria Kondrashova <kavidmi@gmail.com>
2025-11-06 16:16:43 +0300

    feat: add animation

--- clean-uri-landing/src/components/LinksBlock/LinksBlock.tsx
+++ clean-uri-landing/src/components/LinksBlock/LinksBlock.tsx
@@ -1,13 +1,24 @@
 import { ShortenedLink } from '../ShortenedLink/ShortenedLink';
 import { useLinks } from '../../hooks/useLinks';
+import { Flex } from '@chakra-ui/react';
+import "./LinksBlock.scss"
 
 export const LinksBlock = () => {
   const { links } = useLinks();
 
+  if (!links || links.length === 0) {
+    return null;
+  }
+
   return (
-    links &&
-    links.map(el => (
-      <ShortenedLink key={el.id} originalLink={el.originalLink} shortenedLink={el.shortenedLink} />
-    ))
+    <Flex direction="column" gap="8px" className='links-block'>
+      {links.map(el => (
+        <ShortenedLink
+          key={el.id}
+          originalLink={el.originalLink}
+          shortenedLink={el.shortenedLink}
+        />
+      ))}
+    </Flex>
   );
 };
