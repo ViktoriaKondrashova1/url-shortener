12b09a49a60f88ec14c4258ccee15a41c08b3a8e
Viktoria Kondrashova <kavidmi@gmail.com>
2025-11-10 17:31:54 +0300

    fix: fix vercel.json and vite config

--- clean-uri-landing/vite.config.ts
+++ clean-uri-landing/vite.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
     react({
       babel: {
@@ -16,13 +15,21 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  server: {
-    proxy: {
-      '/api/shorten': {
-        target: 'https://cleanuri.com/api/v1',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api\/shorten/, '/shorten'),
-      },
-    },
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
   },
-});
+  server:
+    mode === 'development'
+      ? {
+          proxy: {
+            '/api/shorten': {
+              target: 'https://cleanuri.com/api/v1',
+              changeOrigin: true,
+              rewrite: path => path.replace(/^\/api\/shorten/, '/shorten'),
+            },
+          },
+        }
+      : undefined,
+}));
